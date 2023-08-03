f88cf124da39577d7986db9ae155ad42c57bea8a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-08-03 22:02:59 +0300

    Update
    Updated index page for main__extension.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,37 @@ var swiper = new Swiper(".hero__swiper", {
         },
     }
 });
+
+var swiper = new Swiper(".extension__swiper", {
+    /*navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },*/
+    autoplay: {
+        delay: 1000,
+    },
+    breakpoints: {
+        0: {
+            spaceBetween: 10,
+            slidesPerView: 1,
+            slidesPerColumn: 1,
+        },
+        640: {
+            spaceBetween: 30,
+            slidesPerView: 1,
+            slidesPerColumn: 1,
+        },
+        768: {
+            spaceBetween: 30,
+            slidesPerView: 1,
+            slidesPerColumn: 3,
+        },
+        1024: {
+            spaceBetween: 30,
+            slidesPerView: 3,
+        },
+    }
+});
